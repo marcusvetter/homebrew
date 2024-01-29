28e8945be031bc9d30339d919bac5d28227cfef4
Marcus Vetter <marcus.vetter@live.com>
2024-01-29 12:46:12 +0100

    feat: add freefilesync

---
+++ Casks/freefilesync.rb
@@ -0,0 +1,13 @@
+cask 'freefilesync' do
+    version '13.3'
+    sha256 :no_check
+  
+    url "https://github.com/hkneptune/FreeFileSync/releases/download/v#{version}/FreeFileSync_#{version}_macOS.zip"
+    name 'freefilesync'
+    homepage 'https://github.com/hkneptune/FreeFileSync/releases'
+  
+    auto_updates true
+  
+    pkg "FreeFileSync_#{version}.pkg"
+  
+  end
